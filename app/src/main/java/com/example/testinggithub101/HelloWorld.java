e52cdd1acf998d808b0bb9ef3a574387ed62f648
nitin-jft <nitin.sharma@jellyfishtechnologies.com>
2019-02-13 13:43:43 +0530

    Created HelloWorld Class

---
+++ app/src/main/java/com/example/testinggithub101/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.testinggithub101;
+
+public class HelloWorld {
+
+    String msg;
+    int version;
+
+    public HelloWorld(String msg, int version) {
+        this.msg = msg;
+        this.version = version;
+    }
+}
